154cb1839bcec67f15b536f99c26267c21319af9
emilly015 <emilly.v.duarte01@gmail.com>
2026-03-03 15:01:43 -0300

    feat: criação da API para Editora

---
+++ core/serializers/editora.py
@@ -0,0 +1,8 @@
+from attr import fields
+from core.models.editora import Editora
+from rest_framework.serializers import ModelSerializer
+
+class EditoraSerializer(ModelSerializer):
+    class Meta:
+        model = Editora
+        fields = '__all__'
